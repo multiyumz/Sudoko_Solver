b8b585b608073eff0b82bfa7365f12cf755c8f31
Thomas Johnson <johnsonthomas467@gmail.com>
2022-09-22 10:11:33 +0100

    working script added

--- sudoko-scripts/working.py
+++ sudoko-scripts/working.py
@@ -10,6 +10,50 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+def solve(bo):
+
+    # print(bo)
+
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(bo, i, (row,col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
+def valid(bo, num, pos):
+    # check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
 
 def print_board(bo):
 
@@ -31,4 +75,16 @@ def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
-                return i, j  # row, col
+                return (i, j) # row, col
+    return None
+
+
+print_board(board)
+print("\n")
+print("----------------------")
+print("\n")
+solve(board)
+print("\n")
+print("----------------------")
+print("\n")
+print_board(board)
